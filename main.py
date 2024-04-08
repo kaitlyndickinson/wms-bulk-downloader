a9913fc8ad8c401ccaec2f13df1178b6bebe41fd
Kaitlyn Dickinson <kaitlynnd13@gmail.com>
2024-04-08 16:33:39 -0700

    Map names are properly aligned with buttons

--- main.py
+++ main.py
@@ -31,28 +31,21 @@ for link in map_links:
         map_name = map_param[0].split("/")[-1].split(".")[0]
         maps_info.append((map_name, map_url))
 
+# TODO: Even though we aren't directly using "Map URL",
+# it's probably necessary when we want to get the XML and pass to GDAL.
+
 df = pd.DataFrame(maps_info, columns=["Map Name", "Map URL"])
 
 st.write("## Map Data")
 
-col1, col2, col3 = st.columns([1, 1, 1])
+for index, row in df.iterrows():
+    # Creating columns inside iteration allows them to properly align
+    col1, col2 = st.columns(2)
 
-with col1:
-    st.write("Map URL")
-    for index, row in df.iterrows():
-        st.write(row["Map URL"])
-
-with col2:
-    st.write("Map Name")
-    for index, row in df.iterrows():
+    with col1:
         st.write(row["Map Name"])
 
-with col3:
-    st.write("Transform Data")
-    for index, row in df.iterrows():
-        # TODO: this becomes impossible to tell which button aligns with which map
-        # look into solutions for this, maybe have to change the format we display in
-        
+    with col2:
         if st.button(f"Get Data", key=index):
             # TODO: Instead of writing, save data to session state
             # Go to another page with transform form (or maybe a popup, if possible?)
