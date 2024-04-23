655b17fead3d9aff85c97e7793ed48d943a6be31
Kaitlyn Dickinson <kaitlynnd13@gmail.com>
2024-04-23 14:25:04 -0700

    added progress bar to download and specify save location

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import streamlit as st
-import pandas as pd
+import os
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, parse_qs
-from xml.etree import ElementTree as ET
-
-st.title("WMS Downloader")
 
 # Pull out the map links from the URL
 url = "https://astrowebmaps.wr.usgs.gov/webmapatlas/Layers/maps.html"
@@ -15,7 +12,13 @@ map_links = soup.find_all("a")
 
 maps_info = []
 selected_maps = []
-max_height = 300  
+max_height = 300
+download_folder = "downloaded_maps"
+
+if not os.path.exists(download_folder):
+    os.makedirs(download_folder)
+
+st.title("WMS Downloader")
 
 # Save map names and map URLs for processing
 # TODO: maybe save off planet name (system), layer type, layer, and layer name,
@@ -45,14 +48,22 @@ with st.container(height=max_height):
 
 # Download selected map XML files
 if st.button("Download Maps"):
-    for map_url in selected_maps:
-        response = requests.get(map_url)
+    with st.status("Downloading data..."):
+        for map_url in selected_maps:
+            response = requests.get(map_url)
 
-        if response.status_code == 200:
-            xml_content = response.text
+            if response.status_code == 200:
+                xml_content = response.text
 
-            # Save XML content to a file, TODO: not really sure if this works properly yet
-            file_name = f"{map_url.split('/')[-1].split('.')[0]}.xml"
-            with open(file_name, "w") as f:
-                f.write(xml_content)
-            st.write(f"Downloaded: {file_name}")
+                # Save XML content to a file
+                # TODO: this will most likely fail on the duplicate file names,
+                # should check if it exists first than use a random id or something
+                file_name = f"{map_url.split('/')[-1].split('.')[0]}.xml"
+                file_path = os.path.join(download_folder, file_name)
+
+                try:
+                    with open(file_path, "w") as f:
+                        f.write(xml_content)
+                    st.write(f"Successfully downloaded: {file_name}")
+                except Exception as e:
+                    st.error(f"Error downloading: {file_name}: {e}")
